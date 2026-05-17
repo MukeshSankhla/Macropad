c03e561be1f8215919564a3490cb3c8a8ca0f05f
Mukesh Sankhla <48177784+MukeshSankhla@users.noreply.github.com>
2026-05-17 12:16:01 +0530

    Update boot.py

--- Code/boot.py
+++ Code/boot.py
@@ -1,11 +1,10 @@
 import storage
 import usb_hid
 
-# Remount FIRST before anything else
-# storage.remount("/", readonly=False)
-
-# Then enable HID
 usb_hid.enable(
-    (usb_hid.Device.KEYBOARD,
-     usb_hid.Device.CONSUMER_CONTROL)
+    (
+        usb_hid.Device.KEYBOARD,
+        usb_hid.Device.MOUSE,           # ← this is what's missing
+        usb_hid.Device.CONSUMER_CONTROL,
+    )
 )
